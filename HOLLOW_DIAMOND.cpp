a99e8c3c6dbfbe4d7fd36d87adc7fbd576b759db
Piyush <bishtpiyush610@gamil.com>
2026-02-13 23:33:30 +0530

    Updated hollow diamond logic

--- HOLLOW_DIAMOND.cpp
+++ HOLLOW_DIAMOND.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int n=6;
+    int n=4;
     //TOP
     for (int i=0; i<n; i++){
         for (int j=0; j<n-i-1; j++){ //spaces
@@ -18,14 +18,14 @@ int main(){
         }
         cout<< endl;
     }
-
+    //BOTTOM
     for (int i=0; i<n-1; i++){
-        for (int j=0; j<i+1; j++){
+        for (int j=0; j<i+1; j++){//spaces
             cout<< " ";
         }
         cout<< "*";
 
-        for (int j=0; j<2*(n-2-i)-1; j++){
+        for (int j=0; j<2*(n-2-i)-1; j++){//spaces
             cout<< " ";
         }
         if (i!=n-2){
